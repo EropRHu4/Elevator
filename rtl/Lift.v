17b1faeecd98cce151eac0a5ac7049bcb8e48075
syule <94171817+EropRHu4@users.noreply.github.com>
2022-08-25 15:46:13 +0300

    Update Lift.v
    add DOORS state, fix some mistakes

--- rtl/Lift.v
+++ rtl/Lift.v
@@ -35,9 +35,10 @@ output reg [2:0] elev_f_o,
 
     );
 
-    parameter IDLE = 2'b00,
-              WAIT = 2'b01,
-              MOVE = 2'b10;
+    parameter IDLE     = 2'b00,     // начальное состояние, в которое переходит автомат при rst
+              WAIT     = 2'b01,      // движение лифта до кнопки на пассажира
+              MOVE     = 2'b10,     // движение лифта до этажа, на который нажал пассажир в лифте
+              DOORS    = 2'b11;     // работа дверей
 
 reg [1:0] state, next;
 
@@ -48,13 +49,20 @@ reg [2:0] num_of_floors = 3'b111;   // количество этажей в до
 
 reg butt = 1'b0;
 
-reg [2:0] last_floor = 'bx;
+reg [2:0] time_cnt = 0;
 
-reg [2:0] last_floor2 = 'bx;
+reg [2:0] pass_f_reg = 0;
 
-always @( posedge clk or negedge rst_n ) begin
+reg [2:0] butt_el_reg = 0;
+
+
+always @( posedge clk ) begin
     if ( !rst_n ) state <= IDLE;
-    else          state <= next;
+    else begin
+            state <= next;
+            pass_f_reg <= pass_f == 0 ? pass_f_reg : pass_f;
+            butt_el_reg <= butt_el == 0 ? butt_el_reg : butt_el;
+    end
 end
 
 
@@ -65,64 +73,70 @@ always @( posedge clk ) begin
             busy_o <= 1'b0;
             elev_f_o <= 3'b001;
             doors <= 1'b0;
-            last_floor <= pass_f == 3'b111 ? pass_f - 1'b001 : pass_f + 1'b001;
-            last_floor2 <= butt_el == 3'b111 ? butt_el - 1'b001 : butt_el + 1'b001;
-                        next = WAIT;
+            next = WAIT;
           end
-    WAIT: if ( pass_f ) begin
-                 busy_o <= 1'b1;
-                 butt <= 1'b1;
-                 doors <= 1'b0;
-                 if ( elev_f_o != pass_f && last_floor != pass_f) begin
+
+    WAIT: begin
+            if ( |pass_f || |butt_el )
+                    next = MOVE;
+            else
+                    next = WAIT;
+    end
+
+    MOVE: begin
+            time_cnt <= 1'b0;
+            if ( |pass_f ) begin
+                butt <= 1'b1;
+                if ( elev_f_o != pass_f ) begin
                         elev_f_o <= elev_f_o < pass_f ? elev_f_o + 1 : elev_f_o - 1;
-                        state <= WAIT;
-                        next = state;
-                 end
-                 else if(elev_f_o == pass_f && last_floor2 != butt_el) begin
-                      butt <= 1'b0;
-                      doors <= 1'b1;
-                      last_floor <= pass_f;
                         next = MOVE;
-                 end
-                 else begin 
-                        next = WAIT;
-                        butt <= 1'b0;
-                        doors <= 1'b1;
-                 end
-          end
-          else busy_o <= 1'b0;
-    MOVE: if ( butt_el ) begin
-                 doors <= 1'b0;
-                 busy_o <= 1'b1;
-                 if ( elev_f_o != butt_el && last_floor2 != butt_el) begin
-                        elev_f_o <= elev_f_o < butt_el ? elev_f_o + 1 : elev_f_o - 1;
-                        state <= MOVE;
-                        next = state;
-                 end
-                 else if (elev_f_o == butt_el && last_floor != pass_f) begin
-                        next = WAIT;
-                        busy_o <= 1'b0;
-                        doors <= 1'b1;
-                        last_floor2 <= butt_el;
-                 end
-                 else if (elev_f_o == butt_el && last_floor == pass_f) begin 
-                        next = MOVE;
-                        busy_o <= 1'b0;
-                        doors <= 1'b1;
-                        last_floor2 <= butt_el;
-                 end
-                 else if ( last_floor2 == butt_el && last_floor == pass_f ) begin
-                        next = MOVE;
-                 end
-          end
-          else  begin
-                busy_o <= 1'b0;
-                doors <= 1'b0;
-                        next = WAIT;
-          end
-    default: begin
-                next = IDLE;
-             end
+                end
+                else if( elev_f_o == pass_f ) begin
+                            butt <= 1'b0;
+                            next = DOORS;    
+                end
+            end
+            else if ( |butt_el ) begin
+            butt <= 1'b0;
+                  if ( elev_f_o != butt_el ) begin
+                         elev_f_o <= elev_f_o < butt_el ? elev_f_o + 1 : elev_f_o - 1;
+                         next = MOVE;
+                  end
+                  else if ( elev_f_o == butt_el ) begin
+                              next = DOORS;
+                              busy_o <= 1'b0;
+                  end 
+            end
+            else if ( |butt_el == 0 || |pass_f == 0) next = WAIT; 
+    end
+
+
+    DOORS:      begin
+                    doors <= 1'b1;
+                    if ( time_cnt != 3'b011 ) begin
+                            time_cnt <= time_cnt + 1'b1;
+                            next = DOORS;
+                    end
+                    if ( |pass_f && time_cnt == 3'b011 ) begin
+                            next = WAIT;
+                            doors <= 1'b0;
+                            time_cnt <= 1'b0;
+                    end
+                    else if ( |butt_el && time_cnt == 3'b011) begin
+                            next = WAIT;
+                            doors <= 1'b0;
+                            time_cnt <= 1'b0;
+                    end
+                    else next = DOORS;
+                    if (time_cnt == 3'b011 && (|pass_f == 0 && |butt_el == 0)) begin
+                            doors <= 1'b0;
+                            next = WAIT;
+                    end
+                end
+
+    default:    begin
+                    next = IDLE;
+                end
     endcase
 end
 endmodule
